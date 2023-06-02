8def24f65031500b15fc9d8bc1e1fad7c423b573
Sylvester <sylvester@socialbrothers.nl>
2023-06-02 15:11:00 +0200

    Add Nunito font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Nunito } from 'next/font/google';
+
 import '@/styles/global.css';
 
 import type { ReactNode } from 'react';
@@ -8,9 +10,14 @@ interface RootProps {
   children: ReactNode;
 }
 
+const nunito = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function RootLayout({ children }: RootProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={nunito.className}>
       <body>
         <NextAuthProvider>
           <h1>RootLayout</h1>
