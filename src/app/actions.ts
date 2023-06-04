0f321e37876df5b735ca3660f1d8a7761f27f06d
Sylvester <sylvester@socialbrothers.nl>
2023-06-04 14:37:24 +0200

    Adjust revalidate paths

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -9,46 +9,42 @@ import {
   updateReminderList,
 } from '@/services/ReminderList';
 
-export async function createReminderListAction(formData: FormData, userId: string) {
-  const title = formData.get('title') as string;
-
-  if (typeof title === 'string') {
-    await createReminderList(title, userId);
-  }
-
-  revalidatePath('/');
-}
-
 export async function createReminderAction(formData: FormData, reminderListId: string) {
   const title = formData.get('title') as string;
 
-  if (typeof title === 'string') {
-    await createReminder(title, reminderListId);
-  }
+  await createReminder(title, reminderListId);
 
-  revalidatePath('/');
+  revalidatePath('/dashboard');
+}
+
+export async function createReminderListAction(formData: FormData, userId: string) {
+  const title = formData.get('title') as string;
+
+  await createReminderList(title, userId);
+
+  revalidatePath('/dashboard');
 }
 
 export async function updateReminderAction(id: string, title: string, isCompleted: boolean) {
   await updateReminder(id, title, isCompleted);
 
-  revalidatePath('/');
+  revalidatePath('/dashboard');
 }
 
 export async function updateReminderListAction(id: string, title: string) {
   await updateReminderList(id, title);
 
-  revalidatePath('/');
+  revalidatePath('/dashboard');
 }
 
 export async function deleteReminderAction(id: string) {
   await deleteReminder(id);
 
-  revalidatePath('/');
+  revalidatePath('/dashboard');
 }
 
 export async function deleteReminderListAction(id: string) {
   await deleteReminderList(id);
 
-  revalidatePath('/');
+  revalidatePath('/dashboard');
 }
